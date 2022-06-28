78656b63016bd71929e5ebbde39f4fb39bea3039
root <martaassefa23@gmail.com>
2022-06-28 18:49:07 +0300

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for Degit1 in range(0, 9):
+for Digit1 in range(0, 9):
     for Digit2 in range(Digit1 + 1, 10):
         if Digit1 == 8 and Digit2 == 9:
             print("{}{}".format(Digit1, Digit2))
