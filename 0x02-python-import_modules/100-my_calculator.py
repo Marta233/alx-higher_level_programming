4197e7f8010537d1636fd26d8ec12b21e6eb7c90
root <martaassefa23@gmail.com>
2022-06-30 23:43:32 +0300

    task 7

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     import sys
-
+    from calculator_1 import add, sub, mul, div
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-
-    a = int(sys.arg[1])
-    b = int(sys.arg[3])
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     operator = sys.argv[2]
     if operator == '+':
         result = add(a, b)
@@ -21,4 +19,4 @@ if __name__ == "__main__":
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    print("{} {} {} = {}".format(a, operator, b, result)
+    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result)
