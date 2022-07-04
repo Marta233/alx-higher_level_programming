da0ae5a195baa2caaa43bdfbba66901adb61748d
root <martaassefa23@gmail.com>
2022-07-04 16:28:02 +0300

    task 4

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 and idx > (len(my_list) - 1):
+        return (my_list)
+
+    numbers = my_list.copy()
+    numbers[idx] = element
+    return (numbers)
