c3bf159704bc4d62c66dd8e83c5cbe438954ff05
root <martaassefa23@gmail.com>
2022-07-04 17:49:50 +0300

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,5 +3,6 @@ def new_in_list(my_list, idx, element):
     numbers = my_list.copy()
     if idx < 0 and idx > (len(my_list) - 1):
         return (numbers)
-    numbers[idx] = element
-    return (numbers)
+    else:
+        numbers[idx] = element
+        return (numbers)
