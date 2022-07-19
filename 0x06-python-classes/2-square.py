046689cdede2e6ce8d3de7f47eef905b411d6dfa
root <martaassefa23@gmail.com>
2022-07-19 17:38:37 +0300

    add size value

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
-
+# 2-square.py
+# Marta A Marta
 """Define a class Square."""
 
 
 class Square:
     """Represent a square."""
-
 
     def __init__(self, size=0):
         """Initialize a new Square.
