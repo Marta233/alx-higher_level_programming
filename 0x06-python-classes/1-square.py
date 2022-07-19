7ded27d49784ca72293a3496f6c373b430bf8d3c
root <martaassefa23@gmail.com>
2022-07-19 13:17:48 +0300

    size properties added

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,12 +6,10 @@
 
 class Square:
     """Represent a square."""
-
-
+    
+    
     def __init__(self, size):
         """Initialize a new Square.
-
         Args:
-            size (int): The size of the new square.
-        """
+            size (int): The size of the new square,"""
         self.__size = size
