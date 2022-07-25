a728b6bfaaa7cb92bc5f282f2bae749e81a99e2a
root <martaassefa23@gmail.com>
2022-07-25 14:34:18 +0300

    task 3

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# 2-rectangle.py
+# Marta A
 """Define a Rectangle class."""
 
 
@@ -23,9 +25,9 @@ class Rectangle:
     def height(self, value):
         self.__height = value
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
 
     @property
     def width(self):
