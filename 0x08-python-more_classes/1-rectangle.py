7ed29a471894957c794c2b4b1bf6a5e7d28e1e89
root <martaassefa23@gmail.com>
2022-07-25 14:31:03 +0300

    first task

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,17 +8,17 @@ class Rectangle:
     """Represent a rectangele."""
 
     def __init__(self, width=0, height=0):
-        """Intialize a new rectangle
+        """Intialize a new Rectangle
         Args:
             width (int): the width of the new rectangle.
-            height (int): the width of the new rectangle.
+            height (int): the height of the new rectangle.
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Get/set width of rectangle."""
+        """Get/set width of the rectangle."""
         return self.__width
 
     @width.setter
@@ -31,7 +31,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the current Height of rectangle."""
+        """Get/set the Height of the rectangle."""
         return self.__height
 
     @height.setter
