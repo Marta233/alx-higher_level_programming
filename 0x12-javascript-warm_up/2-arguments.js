a4c389fa5ed02fb11d651315dc85dd9ae3223b7f
root <martaassefa23@gmail.com>
2022-10-03 21:24:55 +0300

    tasks

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const count = process.argv.length - 2;
-if (count === 0){
+const numArgs = process.argv.length - 2;
+if (numArgs === 0) {
   console.log('No argument');
-} else if(count === 1) {
+} else if (numArgs === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
