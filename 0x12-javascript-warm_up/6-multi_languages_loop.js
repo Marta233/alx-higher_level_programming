9cb18bd39df336ce289a95d34d4134978329479d
root <martaassefa23@gmail.com>
2022-10-03 23:10:36 +0300

    tasks

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const strvar = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const strvar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let index = 0; index < strvar.length; index++) {
-  console.log(strvar[index]);
+	console.log(strvar[index]);
 }
